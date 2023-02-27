94f95dfff10e55ddbe500d29bd2ea5fd97df56c3
Charlotte Fusco <ch.fusco104@gmail.com>
2023-02-27 10:14:19 -0600

    Updated links and routes

--- soundscape-app/src/components/Main.jsx
+++ soundscape-app/src/components/Main.jsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './Home'
 import { Menu } from './Menu'
+import { Search } from './Search'
+import { Playlists } from './Playlists'
+import { MusicPlayer } from './MusicPlayer'
 
 export const Main = () => {
   return (
@@ -9,6 +12,9 @@ export const Main = () => {
       <Menu />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/playlists' element={<Playlists />} />
+        <Route path='/music-player' element={<MusicPlayer />} />
       </Routes>
     </div>
   )
--- soundscape-app/src/components/Menu.jsx
+++ soundscape-app/src/components/Menu.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { Home } from "./Home"
 import { Search } from "./Search"
 import { Playlists } from "./Playlists"
@@ -7,10 +8,10 @@ export const Menu = () => {
   return (
     <div className="menu">
       <div className="menu-buttons">
-        <Home />
-        <Search />
-        <Playlists />
-        <MusicPlayer />
+        <Link to='/'>Home</Link>
+        <Link to='/search'>Search</Link>
+        <Link to='/playlists'>Playlists</Link>
+        <Link to='/music-player'>Play Music</Link>
       </div>
     </div>
   )
