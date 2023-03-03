69fc57f28e1906c7b1743ac59c9bfa650b78db8e
Charlotte Fusco <ch.fusco104@gmail.com>
2023-03-03 08:34:25 -0600

    Icon functionality added, some CSS

--- soundscape-app/src/components/Menu.jsx
+++ soundscape-app/src/components/Menu.jsx
@@ -1,19 +1,40 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { Icon } from '@iconify/react';
+import { useState } from 'react';
 
 export const Menu = () => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+
   return (
     <div className="menu">
 
       <div className="menu-buttons">
-        <Link className="home-icon" to='/'>Home</Link>
-        <Link className="inside-home-icon" to='/search'>Search</Link>
-        <Link className="inside-home-icon" to='/playlists'>Playlists</Link>
-        <Link className="inside-home-icon" to='/music-player'>Play Music</Link>
-        <Icon icon="ic:sharp-home" />
-        <Icon icon="ant-design:fast-forward-outlined" />
-        <Icon icon="ant-design:fast-backward-outlined" />
-        <p>VOL</p>
+        <Link
+          className="home-icon"
+          to='/'
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Icon icon="ic:sharp-home" className="home-icon" />
+          {showTooltip && (
+            <span className="tooltip">
+              <Link to='/search'>Search</Link>
+              <Link to='/playlists'>Playlists</Link>
+              <Link to='/music-player'>Play Music</Link>
+            </span>
+          )}
+        </Link>
+        <Icon icon="ant-design:fast-forward-outlined" className="forward-icon" />
+        <Icon icon="ant-design:fast-backward-outlined" className="backward-icon" />
+        <p className="volume-icon">VOL</p>
       </div>
       <div className="button-submit-container">
         <button className='button-submit' type='submit'></button>
